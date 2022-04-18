7a1d56e5362ffe05b7c43dc96045d8a5deb71769
Williamsoditi <williams.oditi@student.moringaschool.com>
2022-04-18 23:48:54 +0300

    Feat : Added page routing to the search components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,13 +2,17 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AboutComponent } from './about/about.component';
 import { NotFoundComponent } from './not-found/not-found.component';
+import { RepoSearchComponent } from './repo-search/repo-search.component';
 import { RepoComponent } from './repo/repo.component';
+import { UserSearchComponent } from './user-search/user-search.component';
 import { UsersComponent } from './users/users.component';
 
 const routes: Routes = [
   { path: 'repo', component: RepoComponent},
   { path: 'about', component: AboutComponent},
   { path: 'users', component: UsersComponent },
+  { path: 'user-search', component: UserSearchComponent},
+  { path: 'repo-search', component: RepoSearchComponent},
   { path: '**', component: NotFoundComponent},
 ];
 
