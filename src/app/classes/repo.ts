60ce0952ef46164ae520d2a361994737b3515858
Williamsoditi <williams.oditi@student.moringaschool.com>
2022-04-18 16:43:36 +0300

    Adds logic in the repo and user classes

--- src/app/classes/repo.ts
+++ src/app/classes/repo.ts
@@ -1,2 +1,14 @@
 export class Repo {
+    constructor(
+        public name:string,
+        public html_url:string ,
+        public description:string,
+        public forks:number,
+        public language:string,
+        public created_at:Date,
+        public watchers_count:number,
+        public homepage: string,
+    ){
+
+    }
 }
