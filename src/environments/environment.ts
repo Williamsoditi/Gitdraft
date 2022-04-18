1f23e0081f6e6841997fd1f1184fe90f61775849
Williamsoditi <williams.oditi@student.moringaschool.com>
2022-04-18 17:04:35 +0300

    Feat: Generates API key and client id

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiKey: 'bbaac509cf3c29c77a11b04c4541af08dd48504b',
+  clientId: '5942a41e935beed90a02',
 };
 
 /*
