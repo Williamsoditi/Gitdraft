6cc38e56617a236d7f19d156bba73d3f205cf6aa
Williamsoditi <williams.oditi@student.moringaschool.com>
2022-04-19 04:33:12 +0300

    Feat: Adds logic to the user-search component

--- src/app/user-search/user-search.component.ts
+++ src/app/user-search/user-search.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
+import { Repo } from '../classes/repo';
+import { User } from '../classes/user';
+import { MainService } from '../main.service';
 
 @Component({
   selector: 'app-user-search',
@@ -6,10 +11,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-search.component.css']
 })
 export class UserSearchComponent implements OnInit {
+  userProfile!: User;
+  UserRepo!: Array<Repo>;
+  repository!: Repo;
+  searchForm: FormGroup = new FormGroup({
+    "username": new FormControl("Williamsoditi", Validators.required),
+  });
 
-  constructor() { }
+
+  constructor( private route: ActivatedRoute, private userService: MainService) { }
+
+  searchResult(){
+    let inputValue = this.searchForm.get('username')?.value;
+    this.userService.profileRequest(inputValue).subscribe((res: User) => {
+      this.userProfile = res;
+    });
+    this.userService.repoRequest(inputValue).subscribe((res:Array<Repo>) => {
+      this.UserRepo = res;
+    });
+  }
 
   ngOnInit(): void {
+    this.searchResult();
   }
 
 }
